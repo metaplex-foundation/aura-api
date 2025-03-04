0826cbc4bb536dc451b50ee5044b2a385e33d31a
n00m4d <grozinok@gmail.com>
2025-03-04 14:47:44 +0100

    feat: add one more miggration which fixes old corrupted data

---
+++ db/ch-migrations/7_migrate_old_data.up.sql
@@ -0,0 +1,22 @@
+ALTER TABLE aura.stats
+UPDATE request_type = CASE
+    WHEN chain = 'solana' THEN 'RPC'
+    WHEN chain = 'solana-das' THEN 'DAS'
+    WHEN chain = 'getProgramAccounts' THEN 'GPA'
+    WHEN chain = 'eclipse' THEN 'RPC'
+    WHEN chain = 'eclipse-das' THEN 'DAS'
+    ELSE 'RPC' -- Default value for any unexpected cases
+END
+where request_type  = '';
+
+alter table aura.stats
+update chain = case
+    WHEN chain = 'solana-das' THEN 'solana'
+    WHEN chain = 'getProgramAccounts' THEN 'solana'
+    WHEN chain = 'eclipse-das' THEN 'eclipse'
+    ELSE chain
+end;
+
+alter table aura.stats
+update method_cost = 13
+where request_type='Websocket' and method_cost=0 and subscription_id=2;
