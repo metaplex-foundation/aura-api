d080d95f2bc44ee9750077d88436978f40298426
Vadim <31490938+n00m4d@users.noreply.github.com>
2025-03-27 14:27:39 +0100

    fix: change env conf name for stats metrics port (#92)

--- pkg/configtypes/config_types.go
+++ pkg/configtypes/config_types.go
@@ -32,7 +32,7 @@ type (
 		CertFile       string   `envconfig:"STATS_API_CERT_FILE" required:"false" split_words:"true"`
 		Port           uint64   `envconfig:"STATS_API_PORT" required:"true" split_words:"true"`
 		SwaggerPort    uint64   `envconfig:"STATS_API_SWAGGER_PORT" required:"true" split_words:"true"`
-		MetricsPort    uint64   `required:"true" split_words:"true"`
+		MetricsPort    uint64   `envconfig:"STATS_API_METRICS_PORT" required:"true" split_words:"true"`
 		AllowedOrigins []string `envconfig:"STATS_API_ALLOWED_ORIGINS" required:"true" split_words:"true"`
 	}
 )
